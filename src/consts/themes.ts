b34d5573f52d6558aecf2778a8736bd3e52dd245
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-11-09 11:42:05 +1100

    Continuing Database Work: Encryption solution TBD
    Going to continue work on my database solution for Haj. Still trying to figure out a lot of stuff regarding encryption. I might just encrypt certain database tables then my database service calls to get that data and decrypts it before the user sees it. I think the next course of action is to finish the database. Run some tests and then work on the authentication and encryption. I really need the onboarding and login flows to work before I can start writing functionality for the application. I might take a brief pause and write up how the application works and the flows required for when everything gets passed to my artist who will do the wireframes

---
+++ src/consts/themes.ts
@@ -0,0 +1,17 @@
+interface Theme {}
+const Dark: Theme = {}
+const Light: Theme = {}
+const DarkSolarized: Theme = {}
+const DarkHighContrast: Theme = {}
+const LightHighContrast: Theme = {}
+const LightSolarized: Theme = {}
+
+export {
+  Theme,
+  Dark,
+  Light,
+  DarkSolarized,
+  DarkHighContrast,
+  LightHighContrast,
+  LightSolarized
+}
